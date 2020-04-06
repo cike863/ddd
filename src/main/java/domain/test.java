07e7a6eda7fe952e2e91d588af5fab16d9507307
zhoulong <cike863@sina.cn>
2020-04-06 21:27:25 +0800

    ddd

--- src/main/java/domain/test.java
+++
@@ -1,6 +0,0 @@
-package domain;
-/**
- * Created by  zhoulong on 2020/4/6.
- */
-public class test {
-}
--- src/main/java/infrastructure/test.java
+++
@@ -1,6 +0,0 @@
-package infrastructure;
-/**
- * Created by  zhoulong on 2020/4/6.
- */
-public class test {
-}
